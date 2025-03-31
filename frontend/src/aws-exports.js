7c553fa80681f709e5f799981d9572e440ed6b7c
Chetan Chandane <chetanchandane10@gmail.com>
2025-03-30 21:36:52 -0400

    Fix Amplify v6 loginWith crash by adding Cognito.loginWith

--- frontend/src/aws-exports.js
+++ frontend/src/aws-exports.js
@@ -3,6 +3,9 @@ const awsconfig = {
       region: process.env.REACT_APP_REGION || 'us-east-1',
       userPoolId: process.env.REACT_APP_USER_POOL_ID,
       userPoolClientId: process.env.REACT_APP_USER_POOL_CLIENT_ID,
+      Cognito: {
+        loginWith: {} 
+      }
     }
   };
   
