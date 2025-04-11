bd4156ffaf1beee40133b8642abd0e0f77141687
Chetan Chandane <chetanchandane10@gmail.com>
2025-04-11 18:32:18 -0400

    discard custom UI components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,23 @@
 import React, { useState } from 'react';
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
-import { signIn, fetchAuthSession } from 'aws-amplify/auth';
+import { Authenticator, AmplifySignOut, withAuthenticator  } from '@aws-amplify/ui-react';
 
 Amplify.configure(awsconfig);
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  const loginAndCallAPI = async () => {
-    try {
-      const user = await signIn({ username: 'testuser', password: 'TestPassword123!'});
-      const session = await fetchAuthSession();
-      const token = session.tokens?.idToken?.toString();
-
-      console.log("JWT:", token);
-      console.log("calling API: ",`${process.env.REACT_APP_API_BASE_URL}/hello`);
-
-      const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/hello`, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      });
-
-      const data = await res.json();
-      setMessage(data.message);
-    } catch (err) {
-      console.error(err);
-      setMessage('Error calling API.');
-    }
-  };
 
   return (
-    <div>
-      <h1>Amplify + Cognito + API Gateway Demo</h1>
-      <button onClick={loginAndCallAPI}>Login & Fetch</button>
-      <p>{message}</p>
+    <Authenticator>
+      <div>
+        <header className="App-header">
+          <h1>Welcome to the Amplify App</h1>
+          <p>This is a simple application using AWS Amplify.</p>
+          <AmplifySignOut />
+        </header>
     </div>
+    </Authenticator>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- terraform/modules/cognito/outputs.tf
+++ terraform/modules/cognito/outputs.tf
@@ -1,12 +1,12 @@
-output "cognito_user_pool_id" {
+output "user_pool_id" {
   value = aws_cognito_user_pool.user_pool.id
 }
 
-output "cognito_user_pool_client_id" {
+output "user_pool_client_id" {
   value = aws_cognito_user_pool_client.user_pool_client.id
 }
 
-output "cognito_identity_pool_id" {
+output "identity_pool_id" {
   value = aws_cognito_identity_pool.identity_pool.id
 }
 
